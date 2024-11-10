b8e7ee07942e49874b3cb9dba8c0ffc0fe949710
Noe Pareti <nopareti@c3r4p6.42perpignan.fr>
2024-11-10 16:13:42 +0100

    home push

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nopareti <nopareti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 15:17:44 by nopareti          #+#    #+#             */
+/*   Updated: 2024/11/10 15:51:07 by nopareti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	ft_printf(const char *str, ...)
@@ -5,76 +17,61 @@ int	ft_printf(const char *str, ...)
 	va_list	args;
 	size_t	i;
 	char	type;
+	int		count;
 
 	va_start(args, str);
 	i = 0;
-	type = 0;
+	count = 0;
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
 			type = str[i + 1];
-			put_format(args, type);
+			count += put_format(args, type);
 			i += 2;
 		}
 		else
 		{
 			ft_putchar_fd(str[i], 1);
+			count++;
 			i++;
 		}
 	}
 	va_end(args);
-	return (0);
+	return (count);
 }
 
-void	put_format(va_list args, char type)
+int	put_format(va_list args, char type)
 {
-	if (type == INT_SYMBOL)
-		ft_putnbr_fd((int) va_arg(args, int), 1);
-	else if (type == C_SYMBOL)
-		ft_putchar_fd((int) va_arg(args, int), 1);
-	else if (type == STR_SYMBOL)
-		ft_putstr_fd((const char *) va_arg(args, const char *), 1);
-	else if (type == '%')
-		ft_putchar_fd(type, 1);
-	else if (type == 'p')
-		ft_putptr((void *) va_arg(args, void *));
-}
+	int	count;
 
-int	is_type(char c)
-{
-	while (*types)
+	count = 0;
+	if (type == 'd' || type == 'i')
+		count += ft_putnbr_custom((int) va_arg(args, int));
+	else if (type == 'c' || type == '%')
 	{
-		if (*types == c)
-			return (1);
+		ft_putchar_fd((int) va_arg(args, int), 1);
+		count++;
 	}
-	return (0);
+	else if (type == 's')
+		count += ft_putstr_custom((char *) va_arg(args, char *));
+	else if (type == 'p')
+		count += ft_putptr((void *) va_arg(args, void *), 0);
+	else if (type == 'u')
+		count += ft_putunsigned((unsigned int) va_arg(args, unsigned int));
+	else if (type == 'x')
+		count += ft_puthex((unsigned int) va_arg(args, unsigned int), 0);
+	else if (type == 'X')
+		count += ft_puthex((unsigned int) va_arg(args, unsigned int), 1);
+	return (count);
 }
-
-void ft_puthex(unsigned long num)
-{
-	if (num >= 16)
-		ft_puthex(num / 16);
-	ft_putchar_fd("0123456789abcdef"[num % 16], 1);
-}
-
-void ft_putptr(void *ptr)
-{
-	unsigned long	adress;
-
-	adress = (unsigned long) ptr;
-	ft_putstr_fd("0x", 1);
-	ft_puthex(adress);
-}
-
+/*
 #include <stdio.h>
 
 int	main(void)
 {
-	const char	s1[] = "BONJOUR";
-	const char	s2[] = "SALUT";
-	int		nb = 456;
-	int	*ptr = &nb;
-	ft_printf("Ceci est une phrase %% : %s, %s, %d, adress : %p", s1, s2, nb, ptr);
+	int value = ft_printf(" %d ", 17);
+	ft_printf("%d", value);
 	return (0);
 }
+*/
--- utils.h
+++ utils.h
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nopareti <nopareti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 15:11:57 by nopareti          #+#    #+#             */
-/*   Updated: 2024/11/10 15:17:36 by nopareti         ###   ########.fr       */
+/*   Created: 2024/11/10 14:51:29 by nopareti          #+#    #+#             */
+/*   Updated: 2024/11/10 15:34:22 by nopareti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef UTILS_H
+# define UTILS_H
 
-# include <stdlib.h>
-# include <string.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <limits.h>
 # include "Libft/libft.h"
-# include "utils.h"
 
-int	ft_printf(const char *str, ...);
-int	is_type(char c);
-int	put_format(va_list args, char type);
+int	ft_putptr(void *ptr, int first);
+int	ft_putnbr_custom(int n);
+int	ft_putstr_custom(char *s);
+int	ft_putunsigned(unsigned int n);
+int	ft_puthex(unsigned int num, int is_upper);
 
 #endif
